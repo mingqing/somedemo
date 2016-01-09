5a29b11aa94e151c752017596ed59bde8422b320
mingqing <mingqing@henji.org>
2016-01-09 11:11:04 +0800

    update

--- testdocx/main.go
+++ testdocx/main.go
@@ -7,26 +7,40 @@ import (
 )
 
 func main() {
-	d := godocx.NewDocXml()
-	d.Test()
+	//d := godocx.NewDocXml()
+
+	/*
+		d.Test()
+		err := d.Save("./data/save/")
+		if err != nil {
+			log.Println("err:", err)
+		}
+	*/
+
+	unpackDocx()
 }
 
-func testDocxFile() {
+func packDocx() {
+	docx, err := godocx.NewDocxFile("demo2.docx")
+	if err != nil {
+		log.Println("err:", err)
+	}
+
+	docxParentDir := "./data/unpack/"
+	err = docx.CombineTo(docxParentDir, "./data/pack/")
+	if err != nil {
+		log.Println("err:", err)
+	}
+}
+
+func unpackDocx() {
 	path := "./data/demo1.docx"
 	docx, err := godocx.NewDocxFileFromPath(path)
 	if err != nil {
 		log.Println("err:", err)
 	}
 
-	/*
-		err = docx.DecomposeTo("./data/unpack/")
-		if err != nil {
-			log.Println("err:", err)
-		}
-	*/
-
-	docxParentDir := "./data/unpack/"
-	err = docx.CombineTo(docxParentDir, "./data/pack/")
+	err = docx.DecomposeTo("./data/unpack/")
 	if err != nil {
 		log.Println("err:", err)
 	}
